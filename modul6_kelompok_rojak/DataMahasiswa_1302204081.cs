fc8a16cb122b27bb3ed0f30eaf9eca84fbfe3c7f
Kevin Febrian <pownyfals@gmail.com>
2022-04-02 16:23:30 +0700

    menambahkan class data mahasiswa 1302204081

---
+++ modul6_kelompok_rojak/DataMahasiswa_1302204081.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace modul6_kelompok_rojak
+{
+    public class DataMahasiswa_1302202072
+    {
+
+        public void readJson()
+        {
+			try
+			{
+            StreamReader r = new StreamReader("jurnal6_1_1302204081.json");
+            string jsonString = r.ReadToEnd();
+            JObject parsed = JObject.Parse(jsonString);
+
+
+            Console.WriteLine("Nama\t: {0} {1}", parsed["firstName"], parsed["lastName"]); ;
+            Console.WriteLine("Kelamin\t: {0}", parsed["gender"]);
+            Console.WriteLine("Umur\t: {0}", parsed["age"]);
+
+            var alamat = parsed["address"];
+            Console.WriteLine("alamat:");
+            Console.WriteLine("\tJalan\t\t: {0}", alamat["streetAddress"]);
+            Console.WriteLine("\tKota\t\t: {0}", alamat["city"]);
+            Console.WriteLine("\tProvinsi\t: {0}", alamat["state"]);
+
+            var mk = parsed["courses"];
+            Console.WriteLine("Matakuliah yang diambil :");
+            foreach (var item in mk)
+            {
+                Console.WriteLine("\t{0}-{1}", item["code"], item["name"]);
+
+            }
+			}catch(Exception ex)
+			{
+                Console.Write(ex.Message);
+			}
+
+        }
+    }
+}
--- modul6_kelompok_rojak/Program.cs
+++ modul6_kelompok_rojak/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul6_kelompok_rojak
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa_1302202072 r = new DataMahasiswa_1302202072();
+            r.readJson();
+
+        }
+    }
+}
